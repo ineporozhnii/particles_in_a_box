36cb8d6669e4e5304890f72cd6af326929f80210
Ihor Neporozhnii <ihor.neporozhnii@mail.utoronto.ca>
2022-07-10 13:07:19 -0700

    Created accelerate function to simulate particle acceleration

--- main.py
+++ main.py
@@ -6,20 +6,26 @@ from matplotlib.animation import FuncAnimation
 
 
 def main():
-    n_atoms = 10
+    n_particles = 10
     box_size_x = 30
     box_size_y = 30
+    elastic_constant = 1.0
     potential = None
+    acceleration_vector = [0.0, -0.01]
+
+    n_frames = 1000
     fig, ax = plt.subplots()
 
     position_limits_x = np.array([-box_size_x/2+box_size_x/10, box_size_x/2-box_size_x/10])
     position_limits_y = np.array([-box_size_y/2+box_size_y/10, box_size_y/2-box_size_y/10])
-    velocity_limits_x = position_limits_x/10
-    velocity_limits_y = position_limits_y/10
+    velocity_limits_x = position_limits_x/100
+    velocity_limits_y = position_limits_y/100
 
-    x_init, y_init, v_x_init, v_y_init = utils.get_init_conditions(n_atoms=n_atoms, position_limits_x=position_limits_x, position_limits_y=position_limits_y, velocity_limits_x=velocity_limits_x, velocity_limits_y=velocity_limits_y)
-    atoms = utils.Atoms(x_init, y_init, v_x_init, v_y_init)
-    ani = FuncAnimation(fig, utils.animate, fargs=(atoms, ax, box_size_x, box_size_y), frames=500, interval=25, repeat=False)
+
+    x_init, y_init, v_x_init, v_y_init = utils.get_init_conditions(n_particles=n_particles, position_limits_x=position_limits_x, position_limits_y=position_limits_y, velocity_limits_x=velocity_limits_x, velocity_limits_y=velocity_limits_y)
+    particles = utils.Particles(x_init, y_init, v_x_init, v_y_init, acceleration_vector[0], acceleration_vector[1])
+    simulation = FuncAnimation(fig, utils.simulate, fargs=(particles, ax, box_size_x, box_size_y, elastic_constant), frames=n_frames, interval=25, repeat=False)
+    #plt.legend()
     plt.show()
 
 
--- utils.py
+++ utils.py
@@ -1,20 +1,23 @@
 import numpy as np
 
-class Atoms():
+class Particles():
     """
-    This class is used to hold atom positions and properties
+    This class holds particle positions, velocities, accelerations, and properties
     """
-    def __init__(self, x, y, v_x, v_y) -> None:
+    def __init__(self, x, y, v_x, v_y, a_x, a_y) -> None:
         self.radii = 5
         self.colors = "cornflowerblue"
         self.x = x
         self.y = y
         self.v_x = v_x
         self.v_y = v_y
+        self.a_x = a_x
+        self.a_y = a_y
 
-def get_init_conditions(n_atoms, position_limits_x, position_limits_y, velocity_limits_x, velocity_limits_y):
+
+def get_init_conditions(n_particles, position_limits_x, position_limits_y, velocity_limits_x, velocity_limits_y):
     """
-    This function creates initial distribution of atomic positions and velocities
+    This function creates initial distribution of particle positions and velocities
     Input:
         n_atoms - number of atoms in the simulations
         position_limits_x - list of low and high limits for initial atomic positions on x axis
@@ -27,36 +30,49 @@ def get_init_conditions(n_atoms, position_limits_x, position_limits_y, velocity_
         v_x_init - initial x velocities 
         v_y_init - initial y velocities 
     """
-    x_init = np.random.uniform(low=position_limits_x[0], high=position_limits_x[1], size=(n_atoms,))
-    y_init = np.random.uniform(low=position_limits_y[0], high=position_limits_y[1], size=(n_atoms,))
-    v_x_init = np.random.uniform(low=velocity_limits_x[0], high=velocity_limits_x[1], size=(n_atoms,))
-    v_y_init = np.random.uniform(low=velocity_limits_y[0], high=velocity_limits_y[1], size=(n_atoms,))
+    x_init = np.random.uniform(low=position_limits_x[0], high=position_limits_x[1], size=(n_particles,))
+    y_init = np.random.uniform(low=position_limits_y[0], high=position_limits_y[1], size=(n_particles,))
+    v_x_init = np.random.uniform(low=velocity_limits_x[0], high=velocity_limits_x[1], size=(n_particles,))
+    v_y_init = np.random.uniform(low=velocity_limits_y[0], high=velocity_limits_y[1], size=(n_particles,))
     return x_init, y_init, v_x_init, v_y_init
+
 
 def update_positions(x, y, v_x, v_y):
     x_new = x + v_x
     y_new = y + v_y
     return x_new, y_new
 
-def animate(i, atom, ax, box_size_x, box_size_y):
-    x_new, y_new = update_positions(atom.x, atom.y, atom.v_x, atom.v_y)
-    
+
+def simulate(frame_idx, particles, ax, box_size_x, box_size_y, elastic_constant):
+    x_new, y_new = update_positions(particles.x, particles.y, particles.v_x, particles.v_y)
+    # Add acceleration 
+    if frame_idx%2 != 0:
+        accelerate(particles)
     # Box boundary collisions
-    atom.v_x[np.where(x_new > box_size_x/2)] = -1 * atom.v_x[np.where(x_new > box_size_x/2)]
-    atom.v_x[np.where(x_new < -box_size_x/2)] = -1 * atom.v_x[np.where(x_new < -box_size_x/2)]
-    atom.v_y[np.where(y_new > box_size_y/2)] = -1 * atom.v_y[np.where(y_new > box_size_y/2)]
-    atom.v_y[np.where(y_new < -box_size_y/2)] = -1 * atom.v_y[np.where(y_new < -box_size_y/2)]
+    particles.v_x[np.where(x_new > box_size_x/2)] = -1 * elastic_constant * particles.v_x[np.where(x_new > box_size_x/2)]
+    particles.v_x[np.where(x_new < -box_size_x/2)] = -1 * elastic_constant * particles.v_x[np.where(x_new < -box_size_x/2)]
+    particles.v_y[np.where(y_new > box_size_y/2)] = -1 * elastic_constant * particles.v_y[np.where(y_new > box_size_y/2)]
+    particles.v_y[np.where(y_new < -box_size_y/2)] = -1 * elastic_constant * particles.v_y[np.where(y_new < -box_size_y/2)]
     x_new[np.where(x_new > box_size_x/2)] = box_size_x/2
     x_new[np.where(x_new < -box_size_x/2)] = -box_size_x/2
     y_new[np.where(y_new > box_size_y/2)] = box_size_y/2
     y_new[np.where(y_new < -box_size_y/2)] = -box_size_y/2
-    
-    atom.x = x_new
-    atom.y = y_new
-    
-    velocities = np.sqrt(atom.v_x**2 + atom.v_y**2)
-
+    # Update particle positions
+    particles.x = x_new
+    particles.y = y_new
+    # Add acceleration 
+    if frame_idx%2 == 0:
+        accelerate(particles)
+    # Calculate velocities
+    velocities = np.sqrt(particles.v_x**2 + particles.v_y**2)
+    # Plot particles
     ax.clear()
-    ax.scatter(atom.x, atom.y, c=velocities, cmap='viridis', s=10/velocities)
+    ax.scatter(particles.x, particles.y, s=100, c=velocities, cmap='viridis', label = f"Average velocity {np.mean(velocities)}")
     ax.set_xlim([-box_size_x/2, box_size_x/2])
     ax.set_ylim([-box_size_y/2, box_size_y/2])
+    print(f"Average velocity {np.mean(velocities)}")
+
+
+def accelerate(particles):
+    particles.v_x = particles.v_x + particles.a_x
+    particles.v_y = particles.v_y + particles.a_y
